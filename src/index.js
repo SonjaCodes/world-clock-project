e0a9e1a534c1dec24613a71db138c0902c9ca987
SonjaCodes <sonja@kolmer.cc>
2024-04-16 20:04:25 +0200

    changed js to reflect drop down menu city instead, same layout

--- src/index.js
+++ src/index.js
@@ -1,35 +1,40 @@
 function updateTime() {
   // Los Angeles
   let losAngelesElement = document.querySelector("#los-angeles");
-  let losAngelesDateElement = losAngelesElement.querySelector(".date");
-  let losAngelesTimeElement = losAngelesElement.querySelector(".time");
-  let losAngelesTime = moment().tz("America/Los_Angeles");
+  if (losAngelesElement) {
+    let losAngelesDateElement = losAngelesElement.querySelector(".date");
+    let losAngelesTimeElement = losAngelesElement.querySelector(".time");
+    let losAngelesTime = moment().tz("America/Los_Angeles");
 
-  losAngelesDateElement.innerHTML = losAngelesTime.format("MMMM Do, YYYY");
-  losAngelesTimeElement.innerHTML = losAngelesTime.format(
-    "h:mm:ss [<small>]A[</small>]"
-  );
+    losAngelesDateElement.innerHTML = losAngelesTime.format("MMMM Do, YYYY");
+    losAngelesTimeElement.innerHTML = losAngelesTime.format(
+      "h:mm:ss [<small>]A[</small>]"
+    );
+  }
 
   // Zurich
   let zurichElement = document.querySelector("#zurich");
-  let zurichDateElement = zurichElement.querySelector(".date");
-  let zurichTimeElement = zurichElement.querySelector(".time");
-  let zurichTime = moment().tz("Europe/Zurich");
+  if (zurichElement) {
+    let zurichDateElement = zurichElement.querySelector(".date");
+    let zurichTimeElement = zurichElement.querySelector(".time");
+    let zurichTime = moment().tz("Europe/Zurich");
 
-  zurichDateElement.innerHTML = zurichTime.format("MMMM Do, YYYY");
-  zurichTimeElement.innerHTML = zurichTime.format(
-    "h:mm:ss [<small>]A[</small>]"
-  );
+    zurichDateElement.innerHTML = zurichTime.format("MMMM Do, YYYY");
+    zurichTimeElement.innerHTML = zurichTime.format(
+      "h:mm:ss [<small>]A[</small>]"
+    );
+  }
 }
 
 function updateCity(event) {
   let cityTimeZone = event.target.value;
+  let cityName = cityTimeZone.replace("_", " ").split("/")[1];
   let cityTime = moment().tz(cityTimeZone);
   let citiesElement = document.querySelector("#cities");
   citiesElement.innerHTML = `
   <div class="city">
   <div>
-          <h2>${cityTimeZone}</h2>
+          <h2>${cityName}</h2>
           <div class="date">${cityTime.format("MMMM Do, YYYY")}</div>
         </div>
         <div class="time">${cityTime.format(
