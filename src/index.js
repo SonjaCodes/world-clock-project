8c8b85c981a8ad880f560b23aa46e385d4408d5f
SonjaCodes <sonja@kolmer.cc>
2024-04-16 19:34:27 +0200

    added html value elements + js function to drop down menu

--- src/index.js
+++ src/index.js
@@ -22,5 +22,24 @@ function updateTime() {
   );
 }
 
+function updateCity(event) {
+  let cityTimeZone = event.target.value;
+  let cityTime = moment().tz(cityTimeZone);
+  let citiesElement = document.querySelector("#cities");
+  citiesElement.innerHTML = `
+  <div class="city">
+  <div>
+          <h2>${cityTimeZone}</h2>
+          <div class="date">${cityTime.format("MMMM Do, YYYY")}</div>
+        </div>
+        <div class="time">${cityTime.format(
+          "h:mm:ss"
+        )} <small>${cityTime.format("A")}</small></div>
+        </div>`;
+}
+
 updateTime();
 setInterval(updateTime, 1000);
+
+let citiesSelectElement = document.querySelector("#city");
+citiesSelectElement.addEventListener("change", updateCity);
